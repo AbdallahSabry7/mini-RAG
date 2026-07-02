8ff7d25ba4dbc993c182ee185dff24653639a54b
Abdallah Sabry <sabryabdallah37@gmail.com>
2026-07-02 04:58:20 +0300

    added the nlp router

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from routes import base_router, data_router
+from routes import base_router, data_router , nlp_router
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from contextlib import asynccontextmanager
 from stores.LLM.LLMProviderFactory import LLMProviderFactory
+from stores.VectorDB.VectorDBFactory import VectorDBFactory
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,14 +18,23 @@ async def lifespan(app: FastAPI):
 
     app.state.embedding_client = llm_factory.create_provider(settings.EMBEDDING_BACKEND)
     app.state.embedding_client.set_embedding(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
+
+    vector_db_factory = VectorDBFactory(config=settings.dict())
+
+    app.state.vector_db_client = vector_db_factory.create_vector_db(settings.VECTOR_DB_BACKEND)
+    app.state.vector_db_client.connect()
+
+
     yield
     app.state.mongodb_client.close()
+    app.state.vector_db_client.disconnect()
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(base_router)
 app.include_router(data_router)
+app.include_router(nlp_router)
 
 
 
